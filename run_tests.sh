7a1c2d652192b7dacd04bf9363ff2c6c8489dbaa
georgiawilder <georgia.w.essig@gmail.com>
2019-11-04 17:21:37 -0500

    keep track of passed

--- run_tests.sh
+++ run_tests.sh
@@ -8,20 +8,28 @@ TESTS=config/g7/test_cases/*
 # Read in m from command line 
 m=$1
 
-# Remove previous output 
-rm output.txt 
+## rm previous output if exists
+echo "Running test cases:" > output.txt
 
+# Variable to keep track of successful 
+correct=0
+# Variable to keep track of total 
+total=0
 for config_file in $TESTS
 do 
     res=$(java mutation.sim.Simulator -p g7 -m $m -c $config_file)
     test=$(echo $config_file | cut -f4 -d/ )
+    let "total += 1"
     if [[ $res == *"Failed"* ]]; 
     then
         echo "Failed on test case "$test >> output.txt
         jaccard=$(echo $res | cut -f2 -d: )
         echo "Jaccard score was: "$jaccard >> output.txt
     else
+        let "correct += 1"
         echo "Succeeded on test case "$test >> output.txt
     fi
 done 
+
+echo "Test cases passed" $correct "/" $total >> output.txt
 
