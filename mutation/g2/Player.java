ebca8ab6834f19b32fce4d7e7ef3f6930c4df300
Kanishk Vashisht <kanishk.vashisht@gmail.com>
2019-11-08 00:07:47 -0500

    kinda works

--- mutation/g2/Player.java
+++ mutation/g2/Player.java
@@ -26,6 +26,7 @@ import mutation.sim.Mutagen;
 
 public class Player extends mutation.sim.Player {
     private Random random;
+    private int numberExperiments = 200;
 
     public Player() {
       Utilities.alert("Hello world");
@@ -40,47 +41,104 @@ public class Player extends mutation.sim.Player {
         return result;
     }
 
-    public Mutagen Play(Console console, int m){
+    public List<Change> collectData(int experiments, Console console){
       List<Change> changes = new ArrayList<Change>();
-      HashMap<String, List<Change>> actionInstances = new HashMap<>();
-
-      for(int i = 0; i < 10; ++ i){
+      //collect experimental data
+      for(int i = 0; i < experiments; ++ i){
         Mutagen result = new Mutagen();
         String genome = randomString();
         String mutated = console.Mutate(genome);
         changes.addAll(Utilities.diff(genome, mutated));
       }
-      System.out.println(changes);
+      return changes;
+    }
 
-      for(Change c: changes){
-        String after = c.after;
-        if(!actionInstances.containsKey(after)){
-          actionInstances.put(after, new ArrayList<Change>());
-        }
-        actionInstances.get(after).add(c);
-      }
+    public Mutagen Play(Console console, int m){
+      List<Change> changes = this.collectData(numberExperiments, console);
+      HashMap<String, List<Change>> artifactChanges = Utilities.sortByAfterArtifact(changes);
+      //unique artifacts
+      List<String> uniqueArtifacts = new ArrayList<String>(artifactChanges.keySet());
 
-      List<String> uniqueActions = new ArrayList<String>(actionInstances.keySet());
+      HashMap<String, List<Change>> actionChanges = new HashMap<>();
+      HashMap<String, Integer> actionCount = new HashMap<>();
 
-      // after contexts
+      // get all possible actions and the changes they're linked to
+      for(String artifact: uniqueArtifacts){
+        List<Change> relatedChanges = artifactChanges.get(artifact);
+        HashMap<Integer, List<String>> contextsByReference = Utilities.getAfterContextByReferencePoint(relatedChanges);
+        HashMap<Integer, String> collapsedWindows = Utilities.collapseContexts(contextsByReference);
+        int bestWindowReference = Utilities.bestWindow(collapsedWindows);
+        String bestWindow = collapsedWindows.get(bestWindowReference);
+        int lengthOfInterest = Utilities.getLastNondisjunctive(bestWindow)+1;
 
-      for(String action: uniqueActions){
-        List<Change> relatedChanges = actionInstances.get(action);
-        System.out.println(action +" : "+relatedChanges.size());
-        HashMap<Integer, List<String>> indexedAfterContext = new HashMap<>(); // {0:[changes at 0], 1:[changes at 1]}
-        List<Integer> contextPoints = new ArrayList<Integer>(relatedChanges.get(0).afterContext.keySet());
-
-        for(Integer index: contextPoints){
-          List<String> contextsAtIndex = new ArrayList<String>();
-          for(Change c: relatedChanges){
-            contextsAtIndex.add(c.afterContext.get(index));
+        // all the best windows results
+        for(String c: contextsByReference.get(bestWindowReference)){
+          String action = c.substring(0, lengthOfInterest);
+          if(!actionChanges.containsKey(action)){
+            actionChanges.put(action, new ArrayList<Change>());
           }
-          System.out.println(Utilities.collapseStrings(contextsAtIndex));
+          if(!actionCount.containsKey(action)){
+            actionCount.put(action, 0);
+          }
+          actionCount.put(action, actionCount.get(action)+1);
         }
-
+        for(Change c: relatedChanges){
+          String action = c.afterContext.get(bestWindowReference).substring(0, lengthOfInterest);
+          c.pointOfInterest = c.location + bestWindowReference;
+          c.pointOfInterest %= 1000;
+          if(c.pointOfInterest < 0){
+            c.pointOfInterest += 1000;
+          }
+          actionChanges.get(action).add(c);
+        }
       }
 
-      return new Mutagen();
+      ArrayList<String> uniqueActions = new ArrayList<String>(actionChanges.keySet());
+      HashMap<List<String>, Integer> solution = new HashMap<>();
+      for(String action: uniqueActions){
+        List<Change> relatedChanges = actionChanges.get(action);
+        relatedChanges = Utilities.deduplicateListChanges(relatedChanges);
+        HashMap<Integer, List<String>> patternContext = new HashMap<>();
+
+        int lookupLength = 10 - action.length();
+        for(int ctr = 0; ctr <= lookupLength; ctr++){
+          patternContext.put(-1*ctr+lookupLength, new ArrayList<String>());
+          for(Change c: relatedChanges){
+            int pointOfInterest = c.pointOfInterest;
+            int startingPoint = pointOfInterest - lookupLength+ctr;
+            if(startingPoint < 0){
+              startingPoint += 1000;
+            }
+            String currentString = "";
+            for(int i = 0; i < 10; i++){
+              int currentPos = startingPoint + i;
+              currentPos %= 1000;
+              currentString += c.beforeGenome.charAt(currentPos)+"";
+            }
+            patternContext.get(-1*ctr+lookupLength).add(currentString);
+          }
+        }
+        HashMap<Integer, String> collapsedWindows = Utilities.collapseContexts(patternContext);
+        int bestWindowReference = Utilities.bestWindow(collapsedWindows);
+        String bestWindow = collapsedWindows.get(bestWindowReference);
+        // System.out.println(bestWindow + " -> " + action);
+        if(patternContext.get(bestWindowReference).size() < 10){
+          continue;
+        }
+        int lengthOfInterest = Utilities.getLastNondisjunctive(bestWindow)+1;
+        // System.out.print(Utilities.formatPattern(patternContext.get(bestWindowReference).get(0).substring(0, lengthOfInterest)) + " -> " + action);
+        // System.out.print(" @ ,  " +patternContext.get(bestWindowReference).size() + " \n");
+        List<String> thisSol = new ArrayList<String>();
+        thisSol.add(Utilities.formatPattern(patternContext.get(bestWindowReference).get(0).substring(0, lengthOfInterest)));
+        thisSol.add(action);
+        solution.put(thisSol, Utilities.getScore(patternContext, bestWindowReference));
+      }
+      System.out.println(solution);
+      Mutagen sol = new Mutagen();
+      for(List<String> s : solution.keySet()){
+        sol.add(s.get(0), s.get(1));
+      }
+      return sol;
     }
 
 }
--- mutation/g2/Utilities.java
+++ mutation/g2/Utilities.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Utilities {
+  static String noSignal = "acgt";
 
   // Wrapper around SOPln to improve readability
   public static <E> void alert(E ... content){
@@ -24,7 +25,6 @@ public class Utilities {
     return withContext;
   }
 
-
   public static List<Change> generateContexts(List<Change> changes, String before, String after){
     List<Change> withContext = new ArrayList<Change>();
     for(Change change: changes){
@@ -38,15 +38,11 @@ public class Utilities {
     int lookupLength = 10 - changeLength;
     int startingPosition = c.location - lookupLength;
     if(startingPosition < 0){
-      startingPosition = 1000 - startingPosition; //wrap around
+      startingPosition = 1000 + startingPosition; //wrap around
     }
-    // System.out.println("lookupLength "+lookupLength);
-    // System.out.println("startingPosition "+startingPosition);
-    // System.out.println("changeLength "+changeLength);
-    // System.out.println("Location "+c.location);
+
     for(int i = 0; i <= lookupLength; i++){
       int start = (startingPosition + i) % 1000;
-      // System.out.println("Start "+start);
       String beforeContext = "";
       String afterContext = "";
       for(int j = 0; j < 10; j++){
@@ -54,10 +50,96 @@ public class Utilities {
         beforeContext += ""+before.charAt(current);
         afterContext += ""+after.charAt(current);
       }
+
+      // weird bug bypass for now
+      if(afterContext.indexOf(c.after) == -1 || afterContext == null){
+        // System.out.println(c +","+ before +","+ after +","+ i +","+ start);
+        System.out.println("lookupLength "+lookupLength);
+        System.out.println("startingPosition "+startingPosition);
+        System.out.println("changeLength "+changeLength);
+        System.out.println("Location "+c.location);
+        System.out.println("Start "+start);
+        System.out.println("Before "+before);
+        System.out.println("After, "+after);
+        System.out.println("AfterContext "+afterContext);
+        System.exit(9);
+        continue;
+      }
+      // weird bug bypass for now
+      if(beforeContext.equals(null)){
+        System.exit(0);
+      }
+      if(beforeContext.indexOf(c.before) == -1 || beforeContext == null){
+        System.out.println("lookupLength "+lookupLength);
+        System.out.println("startingPosition "+startingPosition);
+        System.out.println("changeLength "+changeLength);
+        System.out.println("Location "+c.location);
+        System.out.println("Start "+start);
+        System.out.println("Before "+before);
+        System.out.println("After, "+after);
+        System.out.println("AfterContext "+afterContext);
+        System.exit(9);
+        continue;
+      }
       c.beforeContext.put((i-lookupLength), beforeContext);
       c.afterContext.put((i-lookupLength), afterContext);
     }
     return c;
+  }
+
+
+  // best window is the one with the earliest nondisjunction and ties broken by least ACGTs;
+  public static int bestWindow(HashMap<Integer, String> windows){
+
+    Integer best = -1;
+    int bestND = 10;
+    int bestTrailingACGT = 10;
+    for(Integer loc: new ArrayList<Integer>(windows.keySet())){
+      String[] window = windows.get(loc).split(";");
+
+      //find earliest nondisjunctive element
+      int earliestNondisjunction = 10;
+      for(int i = 0; i < window.length; i++){
+        if(window[i].length() == 1){
+          earliestNondisjunction = i;
+          break;
+        }
+      }
+
+      //find trailing acgt's
+      int trailingACGTs = 0;
+      for(int i = 9; i >= 0; i--){
+        if(!window[i].equals(noSignal)){
+          break;
+        }
+        trailingACGTs+=1;
+      }
+
+      if(earliestNondisjunction < bestND){
+        best = loc;
+        bestND = earliestNondisjunction;
+        bestTrailingACGT = trailingACGTs;
+      }
+
+      if(earliestNondisjunction == bestND && trailingACGTs < bestTrailingACGT){
+        best = loc;
+        bestND = earliestNondisjunction;
+        bestTrailingACGT = trailingACGTs;
+      }
+    }
+    return best;
+  }
+
+  // get last non nondisjunctive element idx
+  public static int getLastNondisjunctive(String swindow){
+    int lastNd = 0;
+    String[] window = swindow.split(";");
+    for(int i = 0; i < window.length; i++){
+      if(window[i].length() == 1){
+        lastNd = i;
+      }
+    }
+    return lastNd;
   }
 
   // consolidate a bunch of single letter changes by location
@@ -70,7 +152,12 @@ public class Utilities {
     Change ongoing = changes.get(0);
     for(Change current : changes.subList(1,changes.size())){
       if(current.location == ongoing.location+ongoing.before.length()){
+        String beforeGenome = ongoing.beforeGenome;
+        String afterGenome = ongoing.afterGenome;
         ongoing = new Change(ongoing.before + current.before, ongoing.after + current.after, ongoing.location);
+        ongoing.beforeGenome = beforeGenome;
+        ongoing.afterGenome = afterGenome;
+
       } else {
         consolidated.add(ongoing);
         ongoing = current;
@@ -91,6 +178,8 @@ public class Utilities {
     for(int ctr = 0; ctr < len; ctr++){
       if(before.charAt(ctr) != after.charAt(ctr)){
         Change c = new Change(before.charAt(ctr)+"", after.charAt(ctr)+"", ctr);
+        c.beforeGenome = before;
+        c.afterGenome = after;
         changes.add(c);
       }
     }
@@ -111,6 +200,62 @@ public class Utilities {
     return maxEntry.getKey();
   }
 
+  // pivot data by after artifact
+  public static HashMap<String, List<Change>> sortByAfterArtifact(List<Change> changes){
+    HashMap<String, List<Change>> actionInstances = new HashMap<>();
+    //sort experimental data by after artifact
+    for(Change c: changes){
+      String after = c.after;
+      if(!actionInstances.containsKey(after)){
+        actionInstances.put(after, new ArrayList<Change>());
+      }
+      actionInstances.get(after).add(c);
+    }
+    return actionInstances;
+  }
+
+  // get hash of after contexts by reference point
+  public static HashMap<Integer, List<String>> getAfterContextByReferencePoint(List<Change> relatedChanges){
+    HashMap<Integer, List<String>> contextsByReferencePoint = new HashMap<>();
+    List<Integer> contextPoints = new ArrayList<Integer>(relatedChanges.get(0).afterContext.keySet());
+    for(Integer index: contextPoints){
+      List<String> contextsAtIndex = new ArrayList<String>();
+      for(Change c: relatedChanges){
+        contextsAtIndex.add(c.afterContext.get(index));
+      }
+      contextsByReferencePoint.put(index, contextsAtIndex);
+      // System.out.println(Utilities.collapseStrings(contextsByReferencePoint.get(index)));
+    }
+    return contextsByReferencePoint;
+  }
+
+  // get hash of before contexts by reference point
+  public static HashMap<Integer, List<String>> getBeforeContextByReferencePoint(List<Change> relatedChanges){
+    HashMap<Integer, List<String>> contextsByReferencePoint = new HashMap<>();
+    List<Integer> contextPoints = new ArrayList<Integer>(relatedChanges.get(0).beforeContext.keySet());
+    for(Integer index: contextPoints){
+      List<String> contextsAtIndex = new ArrayList<String>();
+      for(Change c: relatedChanges){
+        contextsAtIndex.add(c.beforeContext.get(index));
+      }
+      contextsByReferencePoint.put(index, contextsAtIndex);
+      // System.out.println(Utilities.collapseStrings(contextsByReferencePoint.get(index)));
+    }
+    return contextsByReferencePoint;
+  }
+
+  // get hash of collapsed contexts by reference point
+  public static HashMap<Integer, String> collapseContexts(HashMap<Integer, List<String>> contexts){
+
+    List<Integer> contextPoints = new ArrayList<Integer>(contexts.keySet());
+    HashMap<Integer, String> collapsedContexts = new HashMap<>();
+    for(Integer i: contextPoints){
+      collapsedContexts.put(i, collapseStrings(contexts.get(i)));
+    }
+    return collapsedContexts;
+  }
+
+  // collapse a string to its barebones representation
   public static String collapseStrings(List<String> listStrings){
     String collapsed = "";
     int strLength = listStrings.get(0).length();
@@ -122,7 +267,7 @@ public class Utilities {
           chars += currentChar;
         }
       }
-      collapsed += ";"+chars;
+      collapsed += ";"+sortString(chars);
     }
     return collapsed.substring(1);
   }
@@ -130,6 +275,27 @@ public class Utilities {
   public static boolean inString(String haystack, String needle){
     return (haystack.indexOf(needle) >= 0);
   }
+
+  public static String sortString(String inputString) {
+    char tempArray[] = inputString.toCharArray();
+    Arrays.sort(tempArray);
+    return new String(tempArray);
+  }
+
+  //
+  public static List<Change> deduplicateListChanges(List<Change> changes){
+    HashSet<String> seenBefores = new HashSet<String>();
+    List<Change> dedup = new ArrayList<Change>();
+    for(Change c: changes){
+      if(seenBefores.contains(c.beforeGenome)){
+        continue;
+      }
+      dedup.add(c);
+      seenBefores.add(c.beforeGenome);
+    }
+    return dedup;
+  }
+
 
   // format pattern
   public static String formatPattern(String pattern){
@@ -170,6 +336,11 @@ public class Utilities {
     return rules;
   }
 
+  // scoring function
+  public static int getScore(HashMap<Integer, List<String>> patternContext, int bestWindowReference){
+    return patternContext.get(bestWindowReference).size();
+  }
+
 }
 
 class Rule {
@@ -201,7 +372,9 @@ class Rule {
 // ADT for a change type
 class Change implements Comparable<Change>{
   public String before, after;
+  public String beforeGenome, afterGenome;
   public int location;
+  public int pointOfInterest;
   public HashMap<Integer, String> beforeContext = new HashMap<Integer, String>();
   public HashMap<Integer, String> afterContext = new HashMap<Integer, String>();
   static String delimiter = " => ";
