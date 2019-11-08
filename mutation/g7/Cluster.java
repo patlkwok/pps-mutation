da01b1d047109459e4f54cc9391c50750f219072
georgiawilder <georgia.w.essig@gmail.com>
2019-11-08 16:33:21 -0500

    sorting my length in cluster

--- mutation/g7/Cluster.java
+++ mutation/g7/Cluster.java
@@ -9,15 +9,17 @@ import java.util.*;
 public class Cluster{
 
     private static LinkedList<Integer> mutated_bases; 
+    private String mutated; 
 
     public Cluster(String genome, String mutated){
         mutated_bases = new LinkedList<>(); 
         for (int i = 0; i < genome.length(); i++){
             if (genome.charAt(i)!=mutated.charAt(i)) mutated_bases.add(i);
         }
+        this.mutated = mutated; 
     }
 
-    public Map<Integer, LinkedList<Integer>> findWindows(int k) {
+    public Map<Integer, LinkedList<Integer>> findWindows(int k, int numPatterns) {
         Map<Integer, LinkedList<Integer>> clusters = new HashMap<>();
         int[][] distances = new int[mutated_bases.size()][mutated_bases.size()];
         for (int i =0; i < mutated_bases.size(); i++){
@@ -29,8 +31,21 @@ public class Cluster{
             }
         }
         while (clusters.size() > k){
-            Pair<Integer, Integer> min_pair = locateMin(distances);
+            Pair<Integer, Integer> min_pair = locateMin(distances, clusters);
             mergeClusters(min_pair.getKey(), min_pair.getValue(), distances, clusters);
+        }
+        HashMap<Integer, Integer> length_map = new HashMap<>();
+        for(int centroid: clusters.keySet()){
+            int cluster_size = clusters.get(centroid).size(); 
+            if (length_map.containsKey(cluster_size)) length_map.put(cluster_size, length_map.get(cluster_size)+1);
+            else length_map.put(cluster_size, 1);
+            Collections.sort(clusters.get(centroid));
+            //System.out.println(numPatterns);
+            String action = mutated.substring(mutated_bases.get(clusters.get(centroid).get(0)),mutated_bases.get(clusters.get(centroid).get(cluster_size-1))+1);
+            System.out.println(action);
+        }
+        for(int cluster_size : length_map.keySet()){
+            System.out.println("size: " + cluster_size + " has " + length_map.get(cluster_size));
         }
         Map<Integer, LinkedList<Integer>> correct_indices = new HashMap<>();
         for(int index : clusters.keySet()){
@@ -56,24 +71,38 @@ public class Cluster{
             distances[a][i] = new_dist;
             distances[j][a] = 0; 
             distances[a][j] = 0;             
-        }
-        
+        } 
         clusters.remove(j);
         clusters.put(i, i_clust);
-
     }
 
-    private static Pair<Integer, Integer> locateMin(int[][] distances){
+    private static Pair<Integer, Integer> locateMin(int[][] distances, Map<Integer, LinkedList<Integer>> clusters){
         int min = 1000; 
         Pair<Integer, Integer> min_pair = new Pair<>(0,0);
+        HashMap<Pair<Integer, Integer>, Integer> options = new HashMap<>();
         for (int i =0; i < mutated_bases.size(); i++){
             for (int j=0; j < mutated_bases.size(); j++){
-                if (distances[i][j] < min && distances[i][j] != 0){
+                if (distances[i][j] <= min && distances[i][j] != 0){
                     min = distances[i][j];
                     min_pair = new Pair<>(i, j);
+                    if (distances[i][j] == min)
+                        options.put(min_pair, min);
                 }
             }
         }
+        // Return the min pair which merges the smallest clusters 
+        // idea is that it'll merge windows where no overlap occured first (b/c shorter)
+        int cluster_size = Integer.MAX_VALUE; 
+        for (Pair<Integer, Integer> pair : options.keySet()){
+            if (options.get(pair) == min){
+                int size = clusters.get(pair.getKey()).size() + clusters.get(pair.getValue()).size();
+                if (size < cluster_size){
+                    cluster_size = size; 
+                    min_pair = pair; 
+                }
+            }
+        }
+      
         return min_pair;
     }
 
