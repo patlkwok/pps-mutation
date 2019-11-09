0246857ae357062e5f51a04dcfa390e007ddb1cd
zhanzhicheng <554813245@qq.com>
2019-11-08 21:33:38 -0500

    Tournament rules creation

--- mutation/g6/Player.java
+++ mutation/g6/Player.java
@@ -41,7 +41,7 @@ public class Player extends mutation.sim.Player {
         Mutagen result = new Mutagen();
         //result.add("a;c;c", "att");
         //result.add("g;c;c", "gtt");
-        for (int i = 0; i < 1; ++ i) {
+        for (int i = 0; i < 10000; ++ i) {
             String genome = randomString();
             String mutated = console.Mutate(genome);
             char[] input = genome.toCharArray();
